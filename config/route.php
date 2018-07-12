7554d9e28e90bec5d66ebdce8400363b45d344b1
星梦 <hnxsh@foxmail.com>
2018-07-13 00:21:57 +0800

    V1.1.6

---
+++ config/route.php
@@ -0,0 +1,18 @@
+<?php
+
+// =======用户自定义路由规则===========
+
+return array(
+
+    'url_route' => array(
+	
+		//单页路由示例，about-us为后台自定的名称，也可使用单页的内容ID，以下路由在开启伪静态后输出效果： http://127.0.0.1/about-us.html
+        //'home/about-us' => 'home/about/index/scode/about-us', 
+		
+		//列表页路由示例，news为后台自定义的名称，也可以使用栏目编码，以下路由在开启伪静态后输出效果： http://127.0.0.1/news.html
+		//'home/news' => 'home/list/index/scode/news',
+
+       
+    )
+	
+);
