bebf2686f7c8060128a83ea5322d57229dd4db8d
星梦 <hnxsh@foxmail.com>
2018-09-11 15:20:27 +0800

    V1.2.1

--- template/default/js/aoyun.js
+++ template/default/js/aoyun.js
@@ -1,3 +1,7 @@
-if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
-    new WOW().init();
-};
+$(document).ready(function(){
+	//初始化动画
+	if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
+	    new WOW().init();
+	};
+
+});
