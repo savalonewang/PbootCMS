8a942a488f82e31cf12ff834806d3643480502c5
星梦 <hnxsh@foxmail.com>
2018-11-26 22:54:32 +0800

    V1.3.3

--- apps/home/controller/DoController.php
+++ apps/home/controller/DoController.php
@@ -42,9 +42,9 @@ class DoController extends Controller
     {
         if (! ! $id = get('id', 'int')) {
             $this->model->addVisits($id);
-            json(1, 'ok');
+            echo 'var ok;'; // 避免前端浏览器报js错
         } else {
-            json(0, 'error');
+            echo 'var error;'; // 避免前端浏览器报js错
         }
     }
 
@@ -53,9 +53,19 @@ class DoController extends Controller
     {
         if (($id = get('id', 'int')) && ! cookie('likes_' . $id)) {
             $this->model->addLikes($id);
-            cookie('likes_' . $id, true, 31536000);
+            cookie('likes_' . $id, true, 31536000, null, null, null, null);
         }
         location('-1');
+    }
+
+    // 获取是否点赞
+    public function isLikes()
+    {
+        if (($id = get('id', 'int')) && cookie('likes_' . $id)) {
+            return json(1, 'yes');
+        } else {
+            return json(0, 'no');
+        }
     }
 
     // 反对
@@ -63,9 +73,19 @@ class DoController extends Controller
     {
         if (($id = get('id', 'int')) && ! cookie('oppose_' . $id)) {
             $this->model->addOppose($id);
-            cookie('oppose_' . $id, true, 31536000);
+            cookie('oppose_' . $id, true, 31536000, null, null, null, null);
         }
         location('-1');
+    }
+
+    // 获取是否反对
+    public function isOppose()
+    {
+        if (($id = get('id', 'int')) && cookie('oppose_' . $id)) {
+            return json(1, 'yes');
+        } else {
+            return json(0, 'no');
+        }
     }
 }
 
